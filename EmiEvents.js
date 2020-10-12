98a0ac9b82353e544418beb132685d6971939aad
Elmimouni Mohamed <m.elmimouni1998@gmail.com>
2020-10-12 14:09:56 +0100

    first commit

---
+++ EmiEvents.js
@@ -0,0 +1,111 @@
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET","EmiEvents.xml",true);
+xhttp.send();
+function display1(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[0];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display2(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[1];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display3(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[2];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display4(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[3];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display5(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[4];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display6(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[5];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display7(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[6];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display8(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[7];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display9(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[8];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display10(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[9];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display11(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[10];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
+function display12(){
+    
+    var xm = document.getElementById("xm");
+    var x = xm.getElementsByTagName("club")[11];
+    var y = x.getAttributeNode("description");
+    var txt = y.nodeValue;
+
+    alert("Description : " + txt);
+}
